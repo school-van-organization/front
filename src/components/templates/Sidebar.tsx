637aab3419546ef23000751d61a98970ff17c7ab
RichardSantosPaiva <richardpaiva0101@gmail.com>
2025-05-05 00:08:10 -0300

    feat: css implementando no sidebar

--- src/components/templates/Sidebar.tsx
+++ src/components/templates/Sidebar.tsx
@@ -1,58 +1,113 @@
-import React from 'react'
-import userImg from '../assets/user.svg'
+import React, {useState} from 'react'
+import logoImg from '../assets/logo.svg'
 import bagImg from '../assets/bag.svg'
 import payImg from '../assets/pay.svg'
 import editImg from   '../assets/edit.svg'
 import addImg from   '../assets/add.svg'
 import employeeImg from '../assets/employee.svg'
 import notificationsImg from '../assets/notifications.svg'
+import menuImg from '../assets/menu.svg'
+import closeImg from '../assets/close.svg'
+
 import './Sidebar.css'
 
 const Sidebar = () =>{
+
+    const [active, setActive] = useState(false);
+
     return(
+        <>
         <aside className='sidebar'>
             <header className='sidebar-header'>
-                <img src={userImg} alt="" className="logo-img"/>
+                <img src={logoImg} alt="" className='logo' />
             </header>
             <nav>
                 <button>
                     <span>
-                        <img src={bagImg}/>
+                        <img src={bagImg} className='image-button' style={{ fill: 'black' }}/>
                         <span>Home</span>
                     </span>
                 </button>
                 <button>
                     <span>
-                        <img src={payImg} alt="" />
+                        <img src={payImg} alt="" className='image-button'/>
                         <span>Vencimento</span>
                     </span>
                 </button>
                 <button>
                     <span>
-                         <img src={editImg} alt="" />
+                         <img src={editImg} alt="" className='image-button'/>
                         <span>editar</span>
                     </span>
                 </button>
                 <button>
                     <span>
-                        <img src={addImg} alt="" />
+                        <img src={addImg} alt="" className='image-button'/>
                         <span>cadastrar</span>
                     </span>
                 </button>
                 <button>
                     <span>
-                        <img src={employeeImg} alt="" />
+                        <img src={employeeImg} alt="" className='image-button' />
                         <span>monitor</span>
                     </span>
                 </button>
                 <button>
                     <span>
-                        <img src={notificationsImg} alt="" />
+                        <img src={notificationsImg} alt="" className='image-button'/>
                         <span>Notificações</span>
                     </span>
                 </button>
             </nav>
         </aside>
+        <button className="button-mobile"  onClick={()=> setActive(!active)}>
+            <img src={menuImg} />
+            <span>Menu</span>
+        </button>
+        <nav className={`menu-mobile ${active ? 'menu-mobile-active':''} `}>
+            <button >
+                <span >
+                     <img src={closeImg} className="button-mobile"  onClick={()=> setActive(!active)}/>
+                </span>
+            </button>
+            <button>
+                    <span>
+                        <img src={bagImg}/>
+                        <span className="mobile-text">Home</span>
+                    </span>
+                </button>
+                <button>
+                    <span>
+                        <img src={payImg} alt="" />
+                        <span className="mobile-text">Vencimento</span>
+                    </span>
+                </button>
+                <button>
+                    <span>
+                         <img src={editImg} alt="" />
+                        <span className="mobile-text">editar</span>
+                    </span>
+                </button>
+                <button>
+                    <span>
+                        <img src={addImg} alt="" />
+                        <span className="mobile-text">cadastrar</span>
+                    </span>
+                </button>
+                <button>
+                    <span>
+                        <img src={employeeImg} alt="" />
+                        <span className="mobile-text">monitor</span>
+                    </span>
+                </button>
+                <button>
+                    <span>
+                        <img src={notificationsImg} alt="" />
+                        <span className="mobile-text">Notificações</span>
+                    </span>
+                </button>
+        </nav>
+        </>
     )
 } 
 
